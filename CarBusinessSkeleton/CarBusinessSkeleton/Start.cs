457f5a20eb577761832d4378ce6054b9b1e6b34f
Dom2375 <dominic.23773@hotmail.com>
2023-02-27 16:44:51 +0000

    added vehicle class

--- CarBusinessSkeleton/CarBusinessSkeleton/Start.cs
+++ CarBusinessSkeleton/CarBusinessSkeleton/Start.cs
@@ -10,6 +10,30 @@ using System.Windows.Forms;
 
 namespace CarBusinessSkeleton
 {
+    public class VehicleClass
+    {
+        protected string type;
+        protected string make;
+        protected string model;
+        protected int year;
+        protected double price;
+        protected double weight;
+        protected string colour;
+        protected string reg;
+
+        public VehicleClass(string type, string make, string model, int year, double price, double weight, string colour, string reg)
+        //constructor for VehicleClass
+        {
+            this.type = type;
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+            this.weight = weight;
+            this.colour = colour;
+            this.reg = reg;
+        }
+    }
     public partial class Start : Form
     {
         public Start()
